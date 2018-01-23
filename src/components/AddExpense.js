0b4021b7fe3d2e068f85fff907becac4ff9fb740
kipso <kipso.stephin@gmail.com>
2018-01-23 15:08:28 +0530

    Initial Commit

--- src/components/AddExpense.js
+++ src/components/AddExpense.js
@@ -3,17 +3,25 @@ import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
 import { addExpense } from '../actions/expenses';
 
+export class AddExpense extends React.Component {
+	onSubmit = (expense) => {
+		this.props.addExpense(expense);
+		this.props.history.push('/');
+	};
+	render() {
+		return (
+			<div>
+				<h1> Add ExpenseForm </h1>
+				<ExpenseForm  
+					onSubmit={this.onSubmit}
+				/>
+			</div>
+		);
+	}
+}
 
-const AddExpense = (props) => (
-	<div>
-		<h1> Add ExpenseForm </h1>
-		<ExpenseForm  
-			onSubmit={(expense) => {
-				props.dispatch(addExpense(expense));
-				props.history.push('/');
-			}}
-		/>
-	</div>
-);
+const mapDispatchToProps = (dispatch) => ({
+	addExpense: (expense) => dispatch(addExpense(expense))
+});
 
-export default connect()(AddExpense);
+export default connect(undefined, mapDispatchToProps)(AddExpense);
---
+++ src/tests/actions/expenses.test.js
@@ -0,0 +1,51 @@
+import { addExpense, editExpense, removeExpense } from '../../actions/expenses';
+
+test('Should setup remove expense action object', () =>{
+	const action = removeExpense({id:'123as'});
+	expect(action).toEqual({
+		type: 'REMOVE_EXPENSE',
+		id: '123as'
+	});
+});
+
+test('Should setup edit expense action object', () =>{
+	const action = editExpense('123a', {note:'new note value'});
+	expect(action).toEqual({
+		type: 'EDIT_EXPENSE',
+		id: '123a',
+		updates : {
+			note: 'new note value'
+		}
+	});
+});
+
+test('Should setup add expense action object with default values', () => {
+	const action = addExpense();
+	expect(action).toEqual({
+		type:'ADD_EXPENSE',
+		expense:{
+			id:expect.any(String),
+			description:'',
+			amount:0,
+			createdAt:0,
+			note:''
+		}
+	});
+});
+
+test('Should setup add expense action object with provided values', () => {
+	const expenseData = {
+		description:'Rent',
+		amount:1000,
+		createdAt:123467,
+		note:'Last month Rent'
+	};
+	const action = addExpense(expenseData);
+	expect(action).toEqual({
+		type:'ADD_EXPENSE',
+		expense: {
+			...expenseData,
+			id:expect.any(String)
+		}
+	})
+});
