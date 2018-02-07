1f3e9af56b45a01e875bdccd1c3b7905e1bef1be
kipso <kipso.stephin@gmail.com>
2018-02-07 13:41:29 +0530

    Add Expense Value

--- src/components/ExpenseDashboard.js
+++ src/components/ExpenseDashboard.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ExpenseList from './ExpenseList';
 import ExpenseListFilters from './ExpenseListFilter';
+import ExpensesSummary from './ExpensesSummary';
 
 const ExpenseDashboard = () => (
 	<div>
+		<ExpensesSummary />
 		<ExpenseListFilters />
 		<ExpenseList />
 	</div>
