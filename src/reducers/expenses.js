b67c07c8fa500b0abf7b5a3725439a836f756d2e
kipso <kipso.stephin@gmail.com>
2018-02-09 20:17:58 +0530

    Data Fetch From Firebase

--- src/reducers/expenses.js
+++ src/reducers/expenses.js
@@ -24,6 +24,8 @@ const expensesReducer = (state=expensesReducerDefault,action) => {
 					return expense;
 				}
 			});
+		case 'SET_EXPENSES':
+			return action.expenses;
 		default:
 			return state;
 	}
