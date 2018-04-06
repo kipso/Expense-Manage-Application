5fd3600486b0ef7c0fce8887607c933fd966e1a1
kipso <kipso.stephin@gmail.com>
2018-04-07 00:49:41 +0530

    deleting testcases

--- src/tests/components/NotFoundPage.test.js
+++
@@ -1,8 +0,0 @@
- import React from 'react';
- import { shallow } from 'enzyme';
- import NotFound from '../../components/NotFound';
-
- test('should render NotFound correctly',()=>{
- 	const wrapper = shallow(<NotFound />);
- 	expect(wrapper).toMatchSnapshot();
- });
